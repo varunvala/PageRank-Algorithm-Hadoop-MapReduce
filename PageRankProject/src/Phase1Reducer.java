440553f5704d865fb83610642ab46cfa973c09db
VarunVala <varunkumar2205.nitw@gmail.com>
2016-03-27 16:56:38 -0400

    Initial Commit to Hadoop-PageRank Project

---
+++ PageRankProject/src/Phase1Reducer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Phase1Reducer extends Reducer<Text, Text, Text, Text> {
+    
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+        /* 
+         * Output 
+         *     <PageA>    <page-rank of PageA>    PageX,PageY,PageZ,...
+         */
+        
+        boolean first = true;
+        String links = 1 + "\t";
+        for (Text value : values) {
+            if (!first) 
+                links += ",";
+            links += value.toString();
+            first = false;
+        }
+
+        context.write(key, new Text(links));
+    }
+
+}
